f6aec9924a965382ddb249379849696810435c2b
frankvega2010 <franco_vega@msn.com>
2019-05-10 12:22:33 -0300

    Added simple UI and fixed collision

--- JuegoAlgebra/Assets/Game/Scripts/UI/UIButton.cs
+++ JuegoAlgebra/Assets/Game/Scripts/UI/UIButton.cs
@@ -6,12 +6,22 @@ using UnityEngine.UI;
 public class UIButton : MonoBehaviour
 {
     public GameObject player;
+
+    private Steps playerSteps;
+
+    private void Start()
+    {
+        playerSteps = player.GetComponent<Steps>();
+    }
+
     public void Left()
     {
         player.transform.position = player.transform.position + new Vector3(0, 0, -1);
+        playerSteps.addStep();
     }
     public void Right()
     {
         player.transform.position = player.transform.position + new Vector3(0, 0, 1);
+        playerSteps.addStep();
     }
 }
