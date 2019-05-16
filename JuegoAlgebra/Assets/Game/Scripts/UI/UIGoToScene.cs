cd74716005262f63708695d62f05aa0dccd4855d
Franco <franco_vega@msn.com>
2019-05-16 18:27:23 -0300

    Added Menu Scene, New Fonts and UIExtensions

---
+++ JuegoAlgebra/Assets/Game/Scripts/UI/UIGoToScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIGoToScene : MonoBehaviour
+{
+    public string sceneName;
+
+    public void GoToScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
+    }
+}
